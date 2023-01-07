558739db562f2a2c041b8ac6743ef4d2d87cb527
Arthur Grimaud <36770368+arthur-grimaud@users.noreply.github.com>
2023-01-07 11:39:58 +0100

    test 10 x

--- proteoformquant2/run_pfq2_multi_param.sh
+++ proteoformquant2/run_pfq2_multi_param.sh
@@ -8,9 +8,9 @@
 # done
 
 
-MAX_THREADS=16
+MAX_THREADS=30
 
-myArray=(1 2 5 10)
+myArray=(1 2 3 4 5 6 7 8 9 10)
 
 for filename in "Data/"*.mzid; do
     for p in ${myArray[@]}; do
@@ -18,7 +18,7 @@ for filename in "Data/"*.mzid; do
 		file_out=${filename%.mzid}_param_${p//./_}
 		file_out=${file_out##*/}
 		echo $file_out
-	    python3 proteoformquant2.py -i "$filename" -s "${filename%.mzid}.mgf" -o "$file_out" -d Output -max_rank ${p} &
+	    python3 proteoformquant2.py -i "$filename" -s "${filename%.mzid}.mgf" -o "$file_out" -d ../../output_eding_all_10x_2 -max_rank ${p} &
 	    while [ $( jobs | wc -l ) -ge "$MAX_THREADS" ]; do
 		sleep 0.1
 	    done
