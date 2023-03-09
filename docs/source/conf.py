d0339121c021687a8c120232e5d1f322aaf37c9e
Arthur <agrimaud@bmb.sdu.dk>
2023-03-09 14:03:17 +0100

    doc updt

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,11 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Proteoforquant'
+author = 'Arthur Grimaud'
 
-release = '0.1'
-version = '0.1.0'
+release = 'na'
+version = 'na'
 
 # -- General configuration
 
